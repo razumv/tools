7d4768b64bbf6ea8c826e565626cc00a3d1be854
razumv <42905535+razumv@users.noreply.github.com>
2023-07-24 21:27:10 +0200

    Create 3proxy_update.sh

---
+++ 3proxy_update.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+sudo tee <<EOF >/dev/null /etc/3proxy/3proxy.cfg
+nserver 8.8.8.8
+nserver 8.8.4.4
+nserver 1.1.1.1
+nserver 1.0.0.1
+
+nscache 65536
+timeouts 1 5 30 60 180 1800 15 60
+
+external `curl -s ifconfig.me`
+internal `curl -s ifconfig.me`
+
+daemon
+
+log /var/log/3proxy/3proxy.log D
+logformat "- +_L%t.%. %N.%p %E %U %C:%c %R:%r %O %I %h %T"
+rotate 30
+
+auth strong
+
+allow * * * 80-88,8080-8088 HTTP
+allow * * * 443,8443 HTTPS
+
+proxy -p53129 -n -a
+users user:CL:P@ssv0rd
+users none * 47.253.53.46,47.253.81.245
+
+EOF
+
+
+systemctl restart 3proxy
