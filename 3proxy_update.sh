d35450a9cb3065ac0cefd0a28c7e74232d3c12b6
razumv <42905535+razumv@users.noreply.github.com>
2023-07-25 13:53:27 +0200

    Update 3proxy_update.sh

--- 3proxy_update.sh
+++ 3proxy_update.sh
@@ -9,8 +9,8 @@ nserver 1.0.0.1
 nscache 65536
 timeouts 1 5 30 60 180 1800 15 60
 
-external 217.76.54.202
-internal 217.76.54.202
+external `curl -s ifconfig.me`
+internal `curl -s ifconfig.me`
 
 daemon
 
